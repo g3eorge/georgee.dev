7da74e99b0b05aa1a586a25589e33f797caf8dbd
georg-stone <george@thingbomb.com>
2025-01-02 16:55:19 -0500

    init

---
+++ src/pages/blog/rss.xml.js
@@ -0,0 +1,22 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import showdown from "showdown";
+
+let converter = new showdown.Converter();
+
+export async function GET(context) {
+  const posts = await getCollection("blog");
+  return rss({
+    title: `Blog - ${SITE_TITLE}`,
+    description: "George's blog. I'll occasionally post on here if I want to.",
+    site: context.site,
+    items: await Promise.all(
+      posts.map(async (post) => ({
+        ...post.data,
+        link: `/blog/${post.slug}/`,
+        content: converter.makeHtml(post.body),
+        pubDate: post.data.pubDate,
+      }))
+    ),
+  });
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./src/styles/globals.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "class",
+};
